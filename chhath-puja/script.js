3306fa4196ca55b8e35415bd39e7df82445b3a8a
susheel kumar <techiesusheel@gmail.com>
2025-10-10 23:18:19 +0530

    Update script.js

--- chhath-puja/script.js
+++ chhath-puja/script.js
@@ -1,3 +1,13 @@
+// <!-- Google tag (gtag.js) -->
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RSS1QYV7PK');
+
+// ////////////////////////////
+
 const menuBtn = document.getElementById('menu-btn');
 const navbar = document.getElementById('navbar');
 
