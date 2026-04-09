30f9188b042f8522175b980ca575a664217f0a0c
susheel kumar <techiesusheel@gmail.com>
2026-04-09 10:19:03 +0530

    .

--- 0.Single Page/VoiceByKajal/asside/js/launch.js
+++ 0.Single Page/VoiceByKajal/asside/js/launch.js
@@ -1,3 +1,158 @@
+// const canvas = document.getElementById("launchCanvas");
+// const ctx = canvas.getContext("2d");
+
+// const launchKey = "launchAnimationTime";
+// const now = Date.now();
+// const lastLaunch = localStorage.getItem(launchKey);
+
+// // ⏱️ 10 minutes = 600000 ms
+// if (lastLaunch && (now - lastLaunch < 10000)) {
+//     canvas.remove();
+// } else {
+
+// localStorage.setItem(launchKey, now);
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// let particles = [];
+// let stars = [];
+
+// let rocket = {
+//     x: canvas.width / 2,
+//     y: canvas.height,
+//     speed: 7
+// };
+
+
+// // ⭐ STAR BACKGROUND
+// for (let i = 0; i < 80; i++) {
+//     stars.push({
+//         x: Math.random() * canvas.width,
+//         y: Math.random() * canvas.height,
+//         r: Math.random() * 2
+//     });
+// }
+
+// function drawStars(){
+//     ctx.fillStyle="white";
+//     stars.forEach(s=>{
+//         ctx.beginPath();
+//         ctx.arc(s.x,s.y,s.r,0,Math.PI*2);
+//         ctx.fill();
+//     })
+// }
+
+
+// // 💥 FIREWORK
+// function createFirework(x, y) {
+//     for (let i = 0; i < 120; i++) {
+//         particles.push({
+//             x: x,
+//             y: y,
+//             angle: Math.random() * Math.PI * 2,
+//             speed: Math.random() * 6 + 2,
+//             radius: Math.random() * 3 + 1,
+//             life: 100
+//         });
+//     }
+// }
+
+
+// // 🚀 ROCKET
+// function drawRocket() {
+
+//     ctx.beginPath();
+//     ctx.arc(rocket.x, rocket.y, 4, 0, Math.PI * 2);
+//     ctx.fillStyle = "white";
+//     ctx.fill();
+
+//     rocket.y -= rocket.speed;
+
+//     if (rocket.y < canvas.height / 2) {
+//         createFirework(rocket.x, rocket.y);
+//         rocket = null;
+//     }
+// }
+
+
+// function animate() {
+
+//     let grad = ctx.createLinearGradient(0,0,0,canvas.height);
+//     grad.addColorStop(0,"#020111");
+//     grad.addColorStop(1,"#000000");
+
+//     ctx.fillStyle = grad;
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//     drawStars();
+
+//     if (rocket) {
+//         drawRocket();
+//     }
+
+//     particles.forEach((p, i) => {
+
+//         p.x += Math.cos(p.angle) * p.speed;
+//         p.y += Math.sin(p.angle) * p.speed;
+
+//         p.speed *= 0.96;
+//         p.life--;
+
+//         ctx.beginPath();
+//         ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2);
+//         ctx.fillStyle = `hsl(${Math.random()*360},100%,60%)`;
+//         ctx.shadowColor = "white";
+//         ctx.shadowBlur = 8;
+//         ctx.fill();
+
+//         if (p.life <= 0) {
+//             particles.splice(i, 1);
+//         }
+
+//     });
+
+//     requestAnimationFrame(animate);
+// }
+
+
+// window.onload = () => {
+
+//     animate();
+
+//     setTimeout(() => {
+
+//         canvas.style.opacity = "0";
+
+//         setTimeout(()=>{
+//             canvas.remove();
+//         },1500);
+
+//     }, 1800);
+
+// };
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const canvas = document.getElementById("launchCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -5,8 +160,8 @@ const launchKey = "launchAnimationTime";
 const now = Date.now();
 const lastLaunch = localStorage.getItem(launchKey);
 
-// ⏱️ 10 minutes = 600000 ms
-if (lastLaunch && (now - lastLaunch < 1000)) {
+// 10 seconds
+if (lastLaunch && (now - lastLaunch < 100000000)) {
     canvas.remove();
 } else {
 
@@ -24,13 +179,12 @@ let rocket = {
     speed: 7
 };
 
-
-// ⭐ STAR BACKGROUND
-for (let i = 0; i < 80; i++) {
+// ⭐ Stars
+for (let i = 0; i < 800; i++) {
     stars.push({
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
-        r: Math.random() * 2
+        r: Math.random() * 1.2
     });
 }
 
@@ -40,28 +194,25 @@ function drawStars(){
         ctx.beginPath();
         ctx.arc(s.x,s.y,s.r,0,Math.PI*2);
         ctx.fill();
-    })
+    });
 }
 
-
-// 💥 FIREWORK
+// 💥 Firework
 function createFirework(x, y) {
-    for (let i = 0; i < 120; i++) {
+    for (let i = 0; i < 320; i++) {
         particles.push({
-            x: x,
-            y: y,
+            x,
+            y,
             angle: Math.random() * Math.PI * 2,
-            speed: Math.random() * 6 + 2,
-            radius: Math.random() * 3 + 1,
+            speed: Math.random() * 16 + 2,
+            radius: Math.random() * 4 + 1,
             life: 100
         });
     }
 }
 
-
-// 🚀 ROCKET
+// 🚀 Rocket
 function drawRocket() {
-
     ctx.beginPath();
     ctx.arc(rocket.x, rocket.y, 4, 0, Math.PI * 2);
     ctx.fillStyle = "white";
@@ -69,34 +220,31 @@ function drawRocket() {
 
     rocket.y -= rocket.speed;
 
-    if (rocket.y < canvas.height / 2) {
+    if (rocket.y < canvas.height / 2.5) {
         createFirework(rocket.x, rocket.y);
         rocket = null;
     }
 }
 
-
 function animate() {
 
     let grad = ctx.createLinearGradient(0,0,0,canvas.height);
     grad.addColorStop(0,"#020111");
-    grad.addColorStop(1,"#000000");
+    grad.addColorStop(1,"#000000eb");
 
     ctx.fillStyle = grad;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     drawStars();
 
-    if (rocket) {
-        drawRocket();
-    }
+    if (rocket) drawRocket();
 
     particles.forEach((p, i) => {
 
         p.x += Math.cos(p.angle) * p.speed;
         p.y += Math.sin(p.angle) * p.speed;
 
-        p.speed *= 0.96;
+        p.speed *= 1;
         p.life--;
 
         ctx.beginPath();
@@ -106,30 +254,19 @@ function animate() {
         ctx.shadowBlur = 8;
         ctx.fill();
 
-        if (p.life <= 0) {
-            particles.splice(i, 1);
-        }
+        if (p.life <= 0) particles.splice(i, 1);
 
     });
 
     requestAnimationFrame(animate);
 }
 
+// 🚀 Direct start (no delay)
+animate();
 
-window.onload = () => {
-
-    animate();
-
-    setTimeout(() => {
-
-        canvas.style.opacity = "0";
-
-        setTimeout(()=>{
-            canvas.remove();
-        },800);
-
-    }, 1800);
-
-};
+setTimeout(() => {
+    canvas.style.opacity = "0";
+    setTimeout(()=> canvas.remove(), 400);
+}, 1800);
 
 }
